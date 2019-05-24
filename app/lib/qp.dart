d90f5e2f0082c4880d54a80ad52473862735cb0f
Lam Minh Duc <d3rivr@hotspot.glasfaser-ostbayern.de>
2019-05-24 15:20:18 +0200

    qp layout init

--- app/lib/qp.dart
+++ app/lib/qp.dart
@@ -1,19 +1,45 @@
 import 'package:flutter/material.dart';
+import 'util.dart';
 
-void main() => runApp(MyApp());
+class QuestionPage extends StatefulWidget {
+  QuestionPage({Key key, this.title}) : super(key: key);
 
-class MyApp extends StatelessWidget {
+  final String title;
+
+  @override
+  _MyQuestionPageState createState() => _MyQuestionPageState();
+}
+
+class _MyQuestionPageState extends State<QuestionPage> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
+    var questionContainer = new Container(
+      margin: EdgeInsets.all(10.0),
+      padding: EdgeInsets.all(1.0),
+      // width: 300,
+      height: screenAwareSize(200, context),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(),
+      ),
+      child: Text(
+        "Question here!",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: screenAwareSize(40, context), 
+          fontFamily: "Roboto"),
+      ),
+    );
+
+    return new Scaffold(
+      // appBar: new AppBar(
+      //   title: new Text("Stay Hydrated"),
+      // ),
+      body: new ListView(
+        children: <Widget>[
+          questionContainer,
+        ],
       ),
     );
   }
