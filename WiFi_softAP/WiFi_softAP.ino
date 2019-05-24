b2fc42d37774f6ebea97e44e9ad11057b2bb62f6
crazyfunist <langner.johannes@web.de>
2019-05-24 20:46:05 +0200

    versuch2

--- WiFi_softAP/WiFi_softAP.ino
+++ WiFi_softAP/WiFi_softAP.ino
@@ -1,114 +1,106 @@
 #include <WiFi.h>
-#include <WiFiUdp.h>
 
 #define ANSWERTIME 5000
 #define MAXPLAYERS 6
 
 enum {red,blue,green,yellow,purple};
 const char *ssid = "stayhydrated";
+const char *pwd = "123456";
 
 boolean pause = false;
-
-char* packetBuffer[UDP_TX_PACKET_MAX_SIZE + 1];
-String  ReplyBuffer = "OK\r\n";
-
-unsigned int localPort = 8888;      // local port to listen on
 
 int votecount;
 int questionTime;
 bool nextQuestion = true;
 
-WiFiServer server(80);
-IPAddress broadcastIp = WiFi.localIP();
-
 int playerstats[] = {0,0,0,0,0};
 int currentPlayerstats[] = {0,0,0,0,0};
+
+/*
+ * create Server
+ */
+void createServer(){
+
+  Serial.println("Starting server..");
+
+  WiFi.softAP(ssid, pwd, 7);
+
+  IPAddress IP = WiFi.softAPIP();
+
+  Serial.println(IP);
+
+  server.begin();
+
+  Serial.print("Server created\n");
+
+}
+
 
 void setup() {
   delay(1000);
   Serial.begin(115200);
   Serial.println();
-  WiFi.softAP(ssid);
-
-  broadcastIp[3] = 255;
-  
-  IPAddress IP = WiFi.softAPIP();
-  Serial.print("IP address: ");
-  Serial.println(IP);
-  
-  Serial.printf("UDP server on port %d\n", localPort);
-  Udp.begin(localPort);
+  createServer();
 }
 
 void loop() {
-  int packetSize = Udp.parsePacket();
-  if (packetSize) {
-    Serial.printf("Received packet of size %d from %s:%d\n    (to %s:%d, free heap = %d B)\n",
-                  packetSize,
-                  Udp.remoteIP().toString().c_str(), Udp.remotePort(),
-                  Udp.destinationIP().toString().c_str(), Udp.localPort(),
-                  ESP.getFreeHeap());
+  
+  String msg="";//
+  if (msg.equals("STATUS")) {
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    ReplyBuffer =  "OK\r\n";
+    Udp.write(ReplyBuffer);
+    Udp.endPacket();
+  }
+  if (msg.equals("RED")){
+    currentPlayerstats[red]+=1;
+    playerstats[red]+=1;
+    votecount++;
+  }
+  if (msg.equals("BLUE")){
+    currentPlayerstats[blue]+=1;
+    playerstats[blue]+=1;
+    votecount++;
+  }
+  if (msg.equals("GREEN")){
+    currentPlayerstats[green]+=1;
+    playerstats[green]+=1;
+    votecount++;
+  }
+  if (msg.equals("YELLOW")){
+    currentPlayerstats[yellow]+=1;
+    playerstats[yellow]+=1;
+    votecount++;
+  }
+  if (msg.equals("PURPLE")){
+    currentPlayerstats[purple]+=1;
+    playerstats[purple]+=1;
+    votecount++;
+  }
 
-    // read the packet into packetBufffer
-    int n = Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
-    packetBuffer[n] = 0;
-    String msg = "";
-    for(int i = 0; i<n;i++){
-      msg+=packetBuffer[i];
-    }
-    Serial.println("Contents:");
-    Serial.println(String(msg));
+  if(nextQuestion){
+    Udp.beginPacket(broadcastIp);
+    Udp.write("Question/r/n");
+    Udp.endPacket();
+    questionTime = millis();
+    nextQuestion = false;
+  }
+
+  //conditions for result fulfilled
+  if(millis()>questionTime+ANSWERTIME||votecount>=MAXPLAYERS){
     
-    if (msg.equals("STATUS")) {
-      Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-      ReplyBuffer =  "OK\r\n";
-      Udp.write(ReplyBuffer);
-      Udp.endPacket();
-    }
-    if (msg.equals("RED")){
-      currentPlayerstats[red]+=1;
-      votecount++;
-    }
-    if (msg.equals("BLUE")){
-      currentPlayerstats[blue]+=1;
-      votecount++;
-    }
-    if (msg.equals("GREEN")){
-      currentPlayerstats[green]+=1;
-      votecount++;
-    }
-    if (msg.equals("YELLOW")){
-      currentPlayerstats[yellow]+=1;
-      votecount++;
-    }
-    if (msg.equals("PURPLE")){
-      currentPlayerstats[purple]+=1;
-      votecount++;
-    }
-
-    if(nextQuestion){
-      Udp.beginPacket(broadcastIp);
-      Udp.write("Question/r/n");
-      Udp.endPacket();
-      questionTime = millis();
-      nextQuestion = false;
-    }
-
-    //conditions for result fulfilled
-    if(millis()>questionTime+ANSWERTIME||votecount>=MAXPLAYERS){
+    //show voting result, turn rotor etc
+    
+    do{
       
-      //show voting result, turn rotor etc
+      //check if pause
       
-      do{
-        
-        //check if pause
-        
-      } while(pause);
-      
-      nextQuestion = true;
-    }
+    } while(pause);
+    
+    nextQuestion = true;
   }
 }
+
   
   
   
