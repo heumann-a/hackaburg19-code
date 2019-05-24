b2d0bad333b447a5a141e83d7c688f455882ac1c
Ion <ivs5069@gmail.com>
2019-05-24 14:01:49 +0200

    extracted home page out

---
+++ app/lib/homePage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Have some logo here',
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
