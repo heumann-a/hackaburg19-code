da16f7541ca7a736d0684bd3b437f51c19f78d54
crazyfunist <langner.johannes@web.de>
2019-05-25 00:18:42 +0200

    WebSocket implementiert

--- WiFi_softAP/WiFi_softAP.ino
+++ WiFi_softAP/WiFi_softAP.ino
@@ -1,30 +1,33 @@
 #include <WiFi.h>
-#include <WiFiUdp.h>
+#include <WebSocketServer.h>
 
-#define ANSWERTIME 5000
-#define MAXPLAYERS 6
+#define ANSWERTIME 10000 //10 seconds *1000milliseconds
+#define MAXPLAYERS 5
 
-#define UDPBUFSIZE 15
+// #define UDPBUFSIZE 15
 
-enum {red,blue,green,yellow,purple};
+enum {red, blue, green, yellow, purple};
 const char *ssid = "stayhydrated";
 const char *pwd = "123456";
+const int udpBufSize = 15;
+ 
 WiFiServer server(80);
-WiFiUDP udpListener();
-char *udpPuffer[UDPBUFSIZE];
+WebSocketServer webSocketServer;
+byte udpPuffer[udpBufSize];
 boolean standby = false;
 
 int votecount;
 int questionTime;
 bool nextQuestion = true;
+String question = "Wait for the game to start";
 
-int playerstats[] = {0,0,0,0,0};
-int currentPlayerstats[] = {0,0,0,0,0};
+int playerstats[] = {0, 0, 0, 0, 0};
+int currentPlayerstats[] = {0, 0, 0, 0, 0};
 
 /*
- * create Server
- */
-void createServer(){
+  create Server
+*/
+void createServer() {
 
   Serial.println("Starting server..");
 
@@ -46,71 +49,76 @@ void setup() {
   delay(1000);
   Serial.begin(9600);
   Serial.println();
-  createServer();
-
-  Serial.println(udpListener.begin(8888)?"UDP listener started":"failed to start UDP listener");
+  createServer(); // Serial.println(udpListener.begin(8888)?"UDP listener started":"failed to start UDP listener");
 }
 
 void loop() {
-  if(udpListener.parsePacket()>0){
-    udpListener.read(updPuffer,UDPBUFSIZE);
-  }
-  String msg="";
-  for(int i = 0;i<UDPBUFSIZE;i++){
-    msg+=updPuffer[i];
-  }
-  Serial.println(msg);
-  if (msg.equals("STATUS")) {
-    //returnr 'OK' to client
-  }
-  if (msg.equals("RED")){
-    currentPlayerstats[red]+=1;
-    playerstats[red]+=1;
-    votecount++;
-  }
-  if (msg.equals("BLUE")){
-    currentPlayerstats[blue]+=1;
-    playerstats[blue]+=1;
-    votecount++;
-  }
-  if (msg.equals("GREEN")){
-    currentPlayerstats[green]+=1;
-    playerstats[green]+=1;
-    votecount++;
-  }
-  if (msg.equals("YELLOW")){
-    currentPlayerstats[yellow]+=1;
-    playerstats[yellow]+=1;
-    votecount++;
-  }
-  if (msg.equals("PURPLE")){
-    currentPlayerstats[purple]+=1;
-    playerstats[purple]+=1;
-    votecount++;
+  WiFiClient client = server.available();
+ 
+  if (client.connected() && webSocketServer.handshake(client)) {
+
+    String msg;
+    
+    while (client.connected()) {
+ 
+      msg = webSocketServer.getData();
+ 
+      if (data.length() > 0) {
+         Serial.println(msg);
+         if (msg.equals("STATUS")) {
+           webSocketServer.sendData("OK");
+         }
+         if (msg.equals("QUESTION")) {
+           webSocketServer.sendData(question);
+         }
+         if (msg.equals("RED")) {
+           currentPlayerstats[red] += 1;
+           playerstats[red] += 1;
+           votecount++;
+         }
+         if (msg.equals("BLUE")) {
+           currentPlayerstats[blue] += 1;
+           playerstats[blue] += 1;
+           votecount++;
+         }
+         if (msg.equals("GREEN")) {
+           currentPlayerstats[green] += 1;
+           playerstats[green] += 1;
+           votecount++;
+         }
+         if (msg.equals("YELLOW")) {
+           currentPlayerstats[yellow] += 1;
+           playerstats[yellow] += 1;
+           votecount++;
+         }
+         if (msg.equals("PURPLE")) {
+           currentPlayerstats[purple] += 1;
+           playerstats[purple] += 1;
+           votecount++;
+         }
+      }
+ 
+      delay(10); // Delay needed for receiving the data correctly
+   }
+
+  //conditions for result fulfilled
+  if (millis() > questionTime + ANSWERTIME || votecount >= MAXPLAYERS) {
+
+    //show voting result, turn rotor etc
+
+    do {
+
+      //check if standby
+
+    } while (standby);
+
+    nextQuestion = true;
   }
 
-  if(nextQuestion){
-    //push  next question to clients
+  if (nextQuestion) {
+    question = ""
+    
     questionTime = millis();
     nextQuestion = false;
   }
-
-  //conditions for result fulfilled
-  if(millis()>questionTime+ANSWERTIME||votecount>=MAXPLAYERS){
-    
-    //show voting result, turn rotor etc
-    
-    do{
-      
-      //check if standby
-      
-    } while(standby);
-    
-    nextQuestion = true;
-  }
 }
-
-  
-  
-  
-  
